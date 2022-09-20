d8ad53aa07b68becc12012e3bb230a93c0049094
Borovinic <sonja.borovinic@amplitudo.me>
2022-09-20 16:15:46 +0200

    MathCalculations commit

---
+++ src/main/java/exercises/MathCalculations.java
@@ -0,0 +1,21 @@
+package exercises;
+
+public class MathCalculations {
+    public static void main(String[] args) {
+
+        double a = 2.8;
+        double b = 3.1;
+        double c = 6.0;
+        int d = 2;
+        System.out.println(a+b);
+        System.out.println(Math.abs(a));
+        System.out.println(Math.round(a));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.exp(a));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.pow(d,c));
+
+    }
+}
