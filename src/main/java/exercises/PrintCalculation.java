44039a5460a96b5e4cf2d78883a1bfee1b6caf51
Borovinic <sonja.borovinic@amplitudo.me>
2022-09-20 15:14:59 +0200

    PrintCalculation commit

---
+++ src/main/java/exercises/PrintCalculation.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class PrintCalculation {
+    public static void main(String[] args) {
+
+        int a = 299;
+        int b = 800;
+        int sum = a + b;
+
+
+        System.out.println("     " + a);
+        System.out.println("+    " + b);
+        System.out.println("--------");
+        System.out.println("    " + sum);
+
+
+
+    }
+}
