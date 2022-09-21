0884afd54d88d6308e442591bf1a2c7b8978f580
Borovinic <sonja.borovinic@amplitudo.me>
2022-09-21 10:30:24 +0200

    Bigger2 commit

---
+++ src/main/java/exercises/Bigger2.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Bigger2 {
+    public static void main(String[] args) {
+
+        System.out.println("Insert number 1:");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        System.out.println("Insert number 2:");
+        int number2 = scanner.nextInt();
+        scanner.close();
+        if (number1 != number2){
+            System.out.println("Number " + Math.max(number1,number2) + " is bigger than number " + Math.min(number1,number2) + ".");
+        } else {
+            System.out.println("Number " + number1 + " is the same like number " + number2 + ".");
+        }
+
+
+
+
+
+    }
+}
