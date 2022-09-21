ef98eb6f27bd06cbb15249f460ddb9fa5181cb9c
Borovinic <sonja.borovinic@amplitudo.me>
2022-09-21 12:45:32 +0200

    MessageCharacters commit

---
+++ src/main/java/exercises/MessageCharacters.java
@@ -0,0 +1,19 @@
+package exercises;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class MessageCharacters {
+    public static void main(String[] args) {
+
+        System.out.println("Write a message: ");
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        int number =  message.length();
+        if (number>6 && number<10){
+            System.out.println("Your message is: " + message);
+        } else {
+            System.out.println("Your message must be between 6 and 10 characters.");
+        }
+    }
+}
