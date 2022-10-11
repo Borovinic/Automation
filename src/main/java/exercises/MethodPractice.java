0d9352ff221f5f41edbddc590541937a2c7e8465
Borovinic <sonja.borovinic@amplitudo.me>
2022-10-11 10:56:38 +0200

    Math pow commit

---
+++ src/main/java/exercises/MethodPractice.java
@@ -0,0 +1,16 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class MethodPractice {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        System.out.println(powMath(2,4));
+    }
+
+    public static double powMath(int a, int b){
+       double poww = (Math.pow(a,b));
+        return poww;
+    }
+}
